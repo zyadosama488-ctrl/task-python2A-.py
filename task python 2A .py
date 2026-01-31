6840916b537a86198d6c63c858ea8ac7bbf7bb85
zyadosama488-ctrl <zyadosama488@gmail.com>
2026-01-31 04:48:33 +0200

    Add files via upload

---
+++ task python 2A .py
@@ -0,0 +1,10 @@
+temperature = int(input("enter temparature :"))
+if temperature >= 30 :
+  print("its a hot today . stay hydrated")
+elif  20 <= temperature <= 29 :
+  print("its a warm today . enjoy the weather")
+elif 10 <= temperature <= 19 :
+  print("its a cool day . wear a jacket")
+else :
+  print("its a cold day . stay warm")  
+
---
+++ task python 2B.py
@@ -0,0 +1,4 @@
+numbers= list(range(1,21)) 
+for num in numbers :
+    if num % 3 == 0 :
+        print(f'the numbers divisible by 3 :',num)
